2e1b53cdd9dd88e1ac099a9347528bf7a979fd5c
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-15 10:36:49 +0530

    [Abhishek Jat] Add. Map Interface - Find the Key with the Highest Value

---
+++ Day02-Lab_Practice/com/mapinterfaceproblems/findthekeywithhighestvalue/MaxValueKeyFinder.java
@@ -0,0 +1,25 @@
+package com.mapinterfaceproblems.findthekeywithhighestvalue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Class to find the key with the maximum value in a HashMap.
+public class MaxValueKeyFinder {
+	// Method to find the key with the highest value.
+	public static String findMaxKey(HashMap<String, Integer> dataMap) {
+		String maxKey = null;
+		int maxValue = Integer.MIN_VALUE;
+
+		// Iterating through each entry in the map
+		for (Map.Entry<String, Integer> entry : dataMap.entrySet()) {
+			if (entry.getValue() > maxValue) {
+				// Updating maxKey and maxValue if a larger value is found
+				maxValue = entry.getValue();
+				maxKey = entry.getKey();
+			}
+		}
+
+		// Returning the key with the highest value
+		return maxKey;
+	}
+}

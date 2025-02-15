13d3a69b7cb4e7e1b239baebefd9117d74e1285c
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-15 10:23:21 +0530

    [Abhishek Jat] Add. List Interface - Find Frequency of Elements

---
+++ Day02-Lab_Practice/com/listinterfaceproblems/findfrequencyofelements/FindFrequency.java
@@ -0,0 +1,28 @@
+package com.listinterfaceproblems.findfrequencyofelements;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Class to count the frequency of each element in a list of strings.
+public class FindFrequency {
+	// Counts the frequency of each element in a list
+	public static Map<String, Integer> findFrequency(List<String> list) {
+		Map<String, Integer> frequencyMap = new HashMap<>();
+
+		// Loop through each element in the list
+		for (String element : list) {
+			// If the element is already in the map
+			if (frequencyMap.containsKey(element)) {
+				frequencyMap.put(element, frequencyMap.get(element) + 1);
+			}
+			else {
+				// If the element is not in the map
+				frequencyMap.put(element, 1);
+			}
+		}
+
+		// Return the frequency map
+		return frequencyMap;
+	}
+}
---
+++ Day02-Lab_Practice/com/listinterfaceproblems/findfrequencyofelements/FindFrequencyMain.java
@@ -0,0 +1,18 @@
+package com.listinterfaceproblems.findfrequencyofelements;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+// Class to calculate frequency of elements
+public class FindFrequencyMain {
+	public static void main(String[] args) {
+		// Test and print the findFrequency method
+		List<String> list = Arrays.asList("apple", "banana", "apple", "orange");
+
+		// Call the method to find frequencies
+		Map<String, Integer> frequencyMap = FindFrequency.findFrequency(list);
+		System.out.println("Original List: " + list);
+		System.out.println("Frequency Map: " + frequencyMap);
+	}
+}

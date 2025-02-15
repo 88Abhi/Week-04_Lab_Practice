5e20230224be2c481398afb0629d511c2c921377
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-15 10:24:07 +0530

    [Abhishek Jat] Add. List Interface - Remove Duplicates While Preserving Order

---
+++ Day02-Lab_Practice/com/listinterfaceproblems/removeduplicateswhilepreservingorder/RemoveDuplicatesMain.java
@@ -0,0 +1,18 @@
+package com.listinterfaceproblems.removeduplicateswhilepreservingorder;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Class to Check to removal of duplicates
+public class RemoveDuplicatesMain {
+	public static void main(String[] args) {
+		// Test the removeDuplicates method
+		List<Integer> list = Arrays.asList(3, 1, 2, 2, 3, 4);
+
+		// Call the method to remove duplicates in list
+		List<Integer> listWithoutDuplicates = RemoveDuplicates.removeDuplicates(list);
+		System.out.println("List with duplicates: " + list);
+		System.out.println("List without duplicates: " + listWithoutDuplicates);
+	}
+}
+

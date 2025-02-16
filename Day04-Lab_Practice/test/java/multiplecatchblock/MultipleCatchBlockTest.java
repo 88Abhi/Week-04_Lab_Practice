014812b50864b4882835ddbe641552e779d21c8c
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-17 03:04:28 +0530

    [Abhishek Jat] Add. Testing of Multiple Catch Blocks

---
+++ Day04-Lab_Practice/test/java/multiplecatchblock/MultipleCatchBlockTest.java
@@ -0,0 +1,30 @@
+package multiplecatchblock;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+// Class to test the usage of multiple catch block
+class MultipleCatchBlockTest {
+	// Method to test the out of range index in array
+	@Test
+	public void testOfArrayValueOnOutRangeIndex() {
+		// Creating the nums array for test
+		int nums[] = {45, 89};
+
+		// AssertThrow to check weather program returning expected exception or not
+		assertThrows(IndexOutOfBoundsException.class, () -> {
+			throw new IndexOutOfBoundsException("Index is not fessible according size of array");
+		});
+	}
+
+	// Method to test the empty array
+	@Test
+	public void testOfEmptyArray() {
+		int nums[] = {};
+		// AssertThrow to check weather program returning expected exception or not
+		assertThrows(NullPointerException.class, () -> {
+			throw new NullPointerException("Accessing index not fessible because of Empty array");
+		});
+	}
+}

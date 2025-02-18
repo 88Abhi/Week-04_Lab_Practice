9a76f0e24fd06a0b3bc009881ae670df7fdf8356
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-18 16:00:03 +0530

    [Abhishek Jat] Add. Validate a Hex Color Code

---
+++ Day05-Lab_Practice/test/java/regularexpressions/validatehexcolorcodetest/HexColorValidatorTest.java
@@ -0,0 +1,34 @@
+package regularexpressions.validatehexcolorcodetest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.regularexpressions.validatehexcolorcode.HexColorValidator.isValidHexColor;
+
+// Class to test the hex color code
+public class HexColorValidatorTest {
+	// Method to test the hex code for colours
+	@Test
+	public void testOfHexColorValidate() {
+		// Defining test hex color codes
+		String[] testColors = {"#FFA500", "#ff4500", "#123", "#HIJKL", "#abcDEF", "#1A2B3C", "123456"};
+
+		// Defining the expectedColor status to validate the actual status
+		boolean[] expectedColorStatus = {true, true, false, false, true, true, false};
+
+		// Creating a list to store the valid hex color code
+		List<String> validHex = new ArrayList<>();
+
+		// Iterating through testColor and passing it to the HexColorValidator and check isHexColorValidator works fine
+		for (int color = 0; color < testColors.length; color++) {
+			boolean colorValidateStatus = isValidHexColor(testColors[color]);
+			if (colorValidateStatus) validHex.add(testColors[color]);
+
+			// Testing weather given color status is same as expected
+			Assertions.assertEquals(expectedColorStatus[color], colorValidateStatus, "Color code is Invalid");
+		}
+		System.out.println("Valid hex are :" + validHex);
+	}
+}
